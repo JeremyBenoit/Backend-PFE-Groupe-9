57d80df72e3740c8100c8eff70fc1f52fa16f0e7
JeremyBenoit <95240515+JeremyBenoit@users.noreply.github.com>
2022-12-09 14:49:22 +0100

    Update HelloController.java

--- src/main/java/be/vinci/ipl/pokemon_team_maker/HelloController.java
+++ src/main/java/be/vinci/ipl/pokemon_team_maker/HelloController.java
@@ -8,15 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @GetMapping("/hello")
+  @GetMapping("/")
   public String sendHelloWorld(){
-    String message = "Hello AWS Elastic Beanstalk!";
-    try {
-      InetAddress ip = InetAddress.getLocalHost();
-      message += " From host: " + ip;
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    }
-    return message;
+    return "Hello World !";
   }
 }
